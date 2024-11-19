8d2cefdbe4c125a60c062a1e9181ac26d131cb2c
R0XFOR <AndreyTsivis19@gmail.com>
2024-11-19 12:27:18 +0000

    Added Linux support Changed Windows timestamp format

--- backup.bat
+++ backup.bat
@@ -11,7 +11,7 @@ for /f "tokens=2 delims==" %%a in ('wmic OS Get localdatetime /value') do set "d
 set "YY=%dt:~2,2%" & set "YYYY=%dt:~0,4%" & set "MM=%dt:~4,2%" & set "DD=%dt:~6,2%"
 set "HH=%dt:~8,2%" & set "Min=%dt:~10,2%" & set "Sec=%dt:~12,2%"
 
-set "fullstamp=%YYYY%-%MM%-%DD%_%HH%-%Min%-%Sec%"
+set "fullstamp=%DD%-%MM%-%YYYY%_%HH%-%Min%-%Sec%"
 
 if %input_file_extension% == %backup_file_extension% (
 	echo Decrypt
---
+++ backup.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+password="IaPhotograph"
+backup_file_extension=".enc"
+compression=0
+
+
+filename=$(basename -- "$1")
+extension="${filename##*.}"
+filename="${filename%.*}"
+
+timestamp=$(date +%d-%m-%Y_%H-%M-%S)
+
+if [ $extension = "enc" ]
+then
+	echo "Decrypt"
+        openssl aes-256-cbc -d -nosalt -k "$password" -in "$1" -out "backup.zip"
+        7z x -mx$compression backup.zip
+        rm backup.zip
+else
+	echo "Encrypt"
+        7z a -mx$compression backup.zip "$1"
+        openssl aes-256-cbc -e -nosalt -k "$password" -in "backup.zip" -out "backup $timestamp$backup_file_extension"
+        rm backup.zip
+fi
+
+echo "Done!"
